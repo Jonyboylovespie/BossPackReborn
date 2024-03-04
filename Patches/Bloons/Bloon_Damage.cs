75117473acf6509d32ee17bd973b74e89e972607
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2024-03-03 23:59:07 -0500

    Add files via upload

--- Patches/Bloons/Bloon_Damage.cs
+++ Patches/Bloons/Bloon_Damage.cs
@@ -1,49 +1,26 @@
 ﻿using BossIntegration;
-using BossPackReborn.Bosses;
+using BTD_Mod_Helper.Api.Bloons;
 using HarmonyLib;
 using Il2Cpp;
 using Il2CppAssets.Scripts.Simulation.Bloons;
-using Il2CppAssets.Scripts.Simulation.Bloons.Behaviors;
 using Il2CppAssets.Scripts.Simulation.Towers;
 using Il2CppAssets.Scripts.Simulation.Towers.Projectiles;
 
-namespace BossPackReborn.Patches.Bloons;
+namespace BossIntegration.Patches.Bloons;
 
 [HarmonyPatch(typeof(Bloon), nameof(Bloon.Damage))]
 internal class Bloon_Damage
 {
-    [HarmonyPrefix]
-    internal static bool Prefix(Bloon __instance, float totalAmount, Projectile projectile,
+    [HarmonyPostfix]
+    internal static void Postfix(Bloon __instance, float totalAmount, Projectile projectile,
         bool distributeToChildren, bool overrideDistributeBlocker, bool createEffect,
         Tower tower, BloonProperties immuneBloonProperties,
         bool canDestroyProjectile = true, bool ignoreNonTargetable = false,
         bool blockSpawnChildren = false, bool ignoreInvunerable = false)
     {
-        bool result = true;
-
-        if (ModBoss.BossesAlive.TryGetValue(__instance.Id, out var bossInfo))
+        if (ModBoss.Cache.TryGetValue(__instance.bloonModel.id, out var boss))
         {
-            switch (bossInfo.Boss)
-            {
-                case OldDreadBloon oldDreadBloon:
-                    foreach (var flag in oldDreadBloon.Weaknesses[__instance.Id])
-                    {
-                        result = !tower.towerModel.towerSet.HasFlag(flag);
-
-                        if (!result)
-                            break;
-                    }
-                    break;
-                case Damnaticum damnaticum:
-                    __instance.bloonModel.maxHealth += (bossInfo.RoundInfo.tier ?? 1) * totalAmount;
-                    __instance.SetHealth(__instance.bloonModel.maxHealth);
-
-                    result = false;
-                    break;
-                default:
-                    break;
-            }
+            boss.OnDamageMandatory(__instance, totalAmount);
         }
-        return result;
     }
 }
